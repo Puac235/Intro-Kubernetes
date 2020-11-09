b8e60f3c628dcfecc803e565c02572fcbde5f0c0
José Puac <puac235@gmail.com>
2020-11-09 10:48:09 -0600

    Update signup.js

--- web/dist/signup.js
+++ web/dist/signup.js
@@ -5,9 +5,8 @@ function signup()
     var user = document.forms["datos"].elements[0].value;
     var pass = document.forms["datos"].elements[1].value;
 
-    
-    console.log(imagen)
-    var data = {id:user, pass:pass, image:imagen}
+
+    var data = {id:user, pass:pass}
     const Http = new XMLHttpRequest();
     Http.open("POST",`http://34.70.8.211:5000/addUser`, true);
     Http.setRequestHeader("Content-Type", "application/json");
@@ -27,22 +26,4 @@ function signup()
         }
     }
     
-}
-
-
-function capture() 
-{
-    var video = document.getElementById('video');
-    video.pause();
-
-    var canvas = document.getElementById("canvas");
-
-    var contexto = canvas.getContext("2d");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    contexto.drawImage(video, 0, 0, 200, 200);
-
-    var foto = canvas.toDataURL();
-    imagen = foto;
-    video.play();
 }
