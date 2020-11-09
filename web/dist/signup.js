caebe5b7d5e0b223a47eb9add73a8eb09e910d88
José Puac <puac235@gmail.com>
2020-11-09 10:46:55 -0600

    bug

--- web/dist/signup.js
+++ web/dist/signup.js
@@ -4,34 +4,27 @@ function signup()
 {
     var user = document.forms["datos"].elements[0].value;
     var pass = document.forms["datos"].elements[1].value;
-    var passConfirm = document.forms["datos"].elements[2].value;
 
-    if(pass == passConfirm)
-    {
-        console.log(imagen)
-        var data = {id:user, pass:pass, image:imagen}
-        const Http = new XMLHttpRequest();
-        Http.open("POST",`http://34.70.8.211:5000/addUser`, true);
-        Http.setRequestHeader("Content-Type", "application/json");
-        console.log(data);
-        Http.send(JSON.stringify(data));
-        Http.onreadystatechange=function(){
-            if(this.readyState==4 && this.status==200)
-            {
-                var data = JSON.parse(Http.responseText)
-                console.log(data)
-                if (data.status) {
-                    alert("USUARIO CREADO");
-                    window.location = "login.html";
-                } else {
-                    alert("INTENTELO DE NUEVO");
-                }
+    
+    console.log(imagen)
+    var data = {id:user, pass:pass, image:imagen}
+    const Http = new XMLHttpRequest();
+    Http.open("POST",`http://34.70.8.211:5000/addUser`, true);
+    Http.setRequestHeader("Content-Type", "application/json");
+    console.log(data);
+    Http.send(JSON.stringify(data));
+    Http.onreadystatechange=function(){
+        if(this.readyState==4 && this.status==200)
+        {
+            var data = JSON.parse(Http.responseText)
+            console.log(data)
+            if (data.status) {
+                alert("USUARIO CREADO");
+                window.location = "login.html";
+            } else {
+                alert("INTENTELO DE NUEVO");
             }
         }
-    }
-    else
-    {
-        alert("LAS CONTRASEÑAS DEBEN SER IGUALES");
     }
     
 }
